3d4912deb10cbda51a4d56412a47d1767630af3c
itsankitbhusal <ankitbhusal95@gmail.com>
2025-05-08 23:49:54 +0545

    basic modal setup

--- src/components/UserDetailModal/index.tsx
+++ src/components/UserDetailModal/index.tsx
@@ -1,14 +1,26 @@
+import { useOutsideClick } from "@/hooks/useOutsideClick";
 import type { IUser } from "@/types";
+import { useRef } from "react";
 
 interface IProps {
   user?: IUser | null;
   onClose?: () => void;
 }
 const UserDetailModal = ({ user, onClose }: IProps) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+  useOutsideClick(modalRef, () => onClose?.());
+
   return (
-    <div>
-      <div>{JSON.stringify(user)}</div>
-      <button onClick={onClose}>x</button>
+    <div className="modal-wrapper">
+      <div className="modal" ref={modalRef}>
+        <div className="modal-header">
+          <h2>Subscription details</h2>
+          <button onClick={onClose}>x</button>
+        </div>
+        <div className="modal-body">
+          <div>this is test description</div>
+        </div>
+      </div>
     </div>
   );
 };
